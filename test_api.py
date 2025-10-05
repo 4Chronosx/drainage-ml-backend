58c70479a3eed5c145a615aa9b9b68d120bbbc45
4Chronosx <christianjamezz5@gmail.com>
2025-10-05 14:11:58 +0800

    added procfile

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests # type: ignore
 
-url = "http://localhost:8000/predict-25yr"
+url = "https://drainage-ml-backend-production-87a9.up.railway.app/predict-100yr"
 payload = {"point": [1.5, 0.5, 300.0, 0.000014]}
 
 response = requests.post(url, json=payload)
